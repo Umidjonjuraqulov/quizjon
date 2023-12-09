e679c37c0562fdb950936ae00fd4527219ec03b1
Firuz Juraev <62535926+Firuz-JuraevML@users.noreply.github.com>
2023-12-09 23:29:58 +0500

    CREATE TABLES COMPLETED

--- db.sql
+++ db.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
 ); 
 
 
+
 CREATE TABLE IF NOT EXISTS subsciption_type(
   id SERIAL PRIMARY KEY, 
   name VARCHAR(100), 
@@ -17,6 +18,7 @@ CREATE TABLE IF NOT EXISTS subsciption_type(
   duration INT NOT NULL CHECK (duration >= 0), 
   quiz_count INT NOT NULL CHECK (quiz_count >= 0), 
 ); 
+
 
 
 CREATE TABLE IF NOT EXISTS subscription(
@@ -30,6 +32,7 @@ CREATE TABLE IF NOT EXISTS subscription(
 );
 
 
+
 CREATE TABLE IF NOT EXISTS quizzes(
   id SERIAL PRIMARY KEY, 
   user_id INT REFERENCES users(id) ON DELETE SET NULL,  
@@ -37,8 +40,38 @@ CREATE TABLE IF NOT EXISTS quizzes(
 ); 
 
 
-CREATE TABLE IF NOT EXISTS quizzes(
+
+CREATE TABLE IF NOT EXISTS questions(
   id SERIAL PRIMARY KEY, 
-  user_id INT REFERENCES users(id) ON DELETE SET NULL,  
-  name VARCHAR(255)
+  quiz_id INT REFERENCES quizzes(id) ON DELETE SET NULL,  
+  question TEXT, 
+  order_number INT 
 ); 
+
+
+
+CREATE TABLE IF NOT EXISTS answers(
+  id SERIAL PRIMARY KEY, 
+  question_id INT REFERENCES questions(id) ON DELETE SET NULL,  
+  tag VARCHAR(1), 
+  answer TEXT, 
+  correct BOOLEAN NOT NULL
+); 
+
+
+
+CREATE TABLE IF NOT EXISTS results(
+  id SERIAL PRIMARY KEY, 
+  user_id INT REFERENCES users(id) ON DELETE SET NULL, 
+  quiz_id INT REFERENCES quizzes(id) ON DELETE SET NULL,  
+  date TIMESTAMP, 
+  min_order_number INT, 
+  max_order_number INT, 
+  number_questions INT, 
+  time NUMERIC(5, 2), 
+  used_time (5, 2), 
+  correct_answers INT
+); 
+
+
+
