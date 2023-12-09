6d5d62d20bff66d43cc43b0ad526feeb29f5abef
Firuz Juraev <62535926+Firuz-JuraevML@users.noreply.github.com>
2023-12-09 22:50:11 +0500

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY, 
+  name VARCHAR(100), 
+  phone_number VARCHAR(13), 
+  created_at TIMESTAMP, 
+  school VARCHAR(255), 
+  department VARCHAR(255), 
+  login VARCHAR(50) UNIQUE NOT NULL, 
+  password VARCHAR(50) NOT NULL
+); 
 
+
+  
